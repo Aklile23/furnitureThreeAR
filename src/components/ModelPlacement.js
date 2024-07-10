d70b4aa837240cfd78d3bb656d48b9f26d50c917
Aklile <akliletilahun@gmail.com>
2024-07-10 17:35:51 +0300

    place Chair

--- src/components/ModelPlacement.js
+++ src/components/ModelPlacement.js
@@ -2,22 +2,16 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-export const placeModel = (scene, reticle, modelPath, currentModel, setCurrentModel) => {
+export const placeModel = (scene, reticle, currentModel, setCurrentModel) => {
   const loader = new GLTFLoader();
 
   if (reticle.visible) {
-    const cubeGeometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
-    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.setFromMatrixPosition(reticle.matrix);
-    scene.add(cube);
-
-    loader.load(modelPath, (gltf) => {
+    loader.load('/models/chair.glb', (gltf) => {
       if (currentModel) {
         scene.remove(currentModel);
       }
       const model = gltf.scene;
-      model.position.copy(reticle.position);
+      model.position.setFromMatrixPosition(reticle.matrix);
       scene.add(model);
       setCurrentModel(model);
     });
